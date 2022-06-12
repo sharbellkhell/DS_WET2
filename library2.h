6dcf16c00bca9e1a9e1ef295c9cc88977896fe0e
sharbellkhell <88714465+sharbellkhell@users.noreply.github.com>
2022-06-12 18:31:59 +0300

    Add files via upload

--- library2.h
+++ library2.h
@@ -27,7 +27,7 @@ typedef enum {
     INVALID_INPUT = -3
 } StatusType;
 
-/*
+
 void *Init(int k);
 
 StatusType AddEmployee(void *DS, int employeeID, int companyID, int grade);
@@ -40,19 +40,19 @@ StatusType EmployeeSalaryIncrease(void *DS, int employeeID, int salaryIncrease);
 
 StatusType PromoteEmployee(void *DS, int employeeID, int bumpGrade);
 
-StatusType SumOfBumpGradeBetweenTopWorkersByGroup(void *DS, int companyID, int m, void ** sumBumpGrade);
+StatusType SumOfBumpGradeBetweenTopWorkersByGroup(void *DS, int companyID, int m);
 
-StatusType AverageBumpGradeBetweenSalaryByGroup(void *DS, int companyID, int lowerSalary, int higherSalary, void ** averageBumpGrade);
+StatusType AverageBumpGradeBetweenSalaryByGroup(void *DS, int companyID, int lowerSalary, int higherSalary);
 
-StatusType CompanyValue(void *DS, int companyID, void ** standing);
+StatusType CompanyValue(void *DS, int companyID);
 
-
+/*
  * 20 point Bonus function:
 StatusType BumpGradeToEmployees(void *DS, int lowerSalary, int higherSalary, int bumpGrade);
- 
+ */
 
 void Quit(void** DS);
-*/
+
 #ifdef __cplusplus
 }
 #endif
--- testConverter.py
+++ testConverter.py
@@ -5,13 +5,18 @@ with open("test_in_converted", "w") as output_file:
     with open("test.in.txt", "r") as file_1:
         for line in file_1:
             line = line.split()
+            if line[0] == "Init":
+                code_line = "void* ds = "
             if len(line) > 1:
                 args_array = line[1:]
+                if line[0] != "Init":
+                    args_array.insert(0, "ds")
                 args = ",".join(args_array)
-                code_line = line[0] + "(" + args + ");"
+                code_line += line[0] + "(" + args + ");"
             else:
-                code_line = line[0] + "();"
+                code_line = line[0] + "(&ds);"
             output_file.write(" " + code_line + "\n")
+            code_line = ""
     output_file.write("}")
 
 file_1.close()
